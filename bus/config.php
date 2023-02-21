b2bb7a93fa9296f3b9dccaae8c8e89a2d23f9254
mikeyolang <brunomike965@gmail.com>
2023-02-21 23:18:44 +0300

    The initial commit of the whole project

---
+++ bus/config.php
@@ -0,0 +1,7 @@
+<?php
+$title = 'Online Ticket Reservation System';
+//E-Ticketing System For Railway
+$supervisor_name = "Dr.Bosire";
+$developer_name = "Peter Ndungu Samuel Mwendwa Lawrence Juma  Alfred Gaitho
+Peter Kamau Joshua Mannaseh";
+
